56c7e98490f3609a0c8489f1e9e1bccae483c8b1
snehaalboina <sa217@zips.uakron.edu>
2016-05-13 15:05:08 -0400

    changed the paramters

--- src/Video.cpp
+++ src/Video.cpp
@@ -7,8 +7,8 @@
 #include "Video.hpp"
 
 // constructor
-Video::Video(const std::string& title, int price_code)
-    : title(title), price_code(price_code)
+Video::Video(const std::string& title, int price_cd)
+    : title(title), price_code(price_cd)
 { }
 
 // movie title
@@ -20,11 +20,11 @@ const std::string& Video::getTitle() const {
 // movie price code
 int Video::getCode() const {
 
-    return price_code;
+    return price_cd;
 }
 
 // set movie price code
 void Video::setPriceCode(int new_price_code) {
 
-    price_code = new_price_code;
+    price_cd = new_price_code;
 }
